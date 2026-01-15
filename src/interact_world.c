5c12600acbc4ad6898bf9a1ba819ca1f3c36ae64
Antti Pollanen <antti.pollanen89@gmail.com>
2026-01-15 16:41:17 +0200

    Fixed some norm and added helper function to moving objects

--- src/interact_world.c
+++ src/interact_world.c
@@ -6,11 +6,27 @@
 /*   By: anpollan <anpollan@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/02 15:41:46 by anpollan          #+#    #+#             */
-/*   Updated: 2026/01/09 16:36:52 by anpollan         ###   ########.fr       */
+/*   Updated: 2026/01/15 16:37:27 by anpollan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
+
+ static void	set_object_new_position(t_app *app, t_camera *c, t_ray r)
+{
+	t_vector	new_pos;
+
+	new_pos.x = c->from.x + (r.direction.x * app->selected_object_t);
+	new_pos.y = c->from.y + (r.direction.y * app->selected_object_t);
+	new_pos.z = c->from.z + (r.direction.z * app->selected_object_t);
+	app->selected_object->transform.data[0][3] =
+		new_pos.x + app->selected_object_offset.x;
+	app->selected_object->transform.data[1][3] =
+		new_pos.y + app->selected_object_offset.y;
+	app->selected_object->transform.data[2][3] =
+		new_pos.z + app->selected_object_offset.z;
+	set_transform(app->selected_object, app->selected_object->transform);
+}
 
 static t_vector	get_selected_object_offest(t_app *app, t_ray r, t_intersection hit)
 {
@@ -35,23 +51,17 @@ void	move_oject_on_screen(t_app *app)
 	int			x;
 	int			y;
 	t_ray		r;
-	t_vector	new_pos;
 	t_camera	*c;
 
 	if (!app->selected_object)
 		return ;
 	c = app->scene->camera;
 	mlx_get_mouse_pos(app->mlx, &x, &y);
-	if (doubles_are_equal(app->prev_mouse_x, x) && doubles_are_equal(app->prev_mouse_y, y))
+	if (doubles_are_equal(app->prev_mouse_x, x)
+		&& doubles_are_equal(app->prev_mouse_y, y))
 		return ;
-	r = ray_for_pixel(app->scene->camera, x, y);
-	new_pos.x = c->from.x + (r.direction.x * app->selected_object_t);
-	new_pos.y = c->from.y + (r.direction.y * app->selected_object_t);
-	new_pos.z = c->from.z + (r.direction.z * app->selected_object_t);
-	app->selected_object->transform.data[0][3] = new_pos.x + app->selected_object_offset.x;
-	app->selected_object->transform.data[1][3] = new_pos.y + app->selected_object_offset.y;
-	app->selected_object->transform.data[2][3] = new_pos.z + app->selected_object_offset.z;
-	set_transform(app->selected_object, app->selected_object->transform);
+	r = ray_for_pixel(c, x, y);
+	set_object_new_position(app, c, r);
 	app->data_changed = true;
 }
 
