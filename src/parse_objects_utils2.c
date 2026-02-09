9d0870951d2c220b3b253596eb6435ddd5c2fe6b
Antti Pollanen <antti.pollanen89@gmail.com>
2026-02-09 13:47:14 +0200

    Fixed plane, cylinder and cone orientations

--- src/parse_objects_utils2.c
+++ src/parse_objects_utils2.c
@@ -16,25 +16,28 @@ void	get_cylinder_data(char **line, t_specs *s, t_app *app)
 {
 	skip_whitespace(line);
 	if (!parse_point(line, &s->position))
-		parse_error("Invalid cylinder position", app);
+		parse_error("Invalid cylinder or cone position", app);
 	skip_whitespace(line);
 	if (!parse_vector(line, &s->axis) || !is_valid_axis(s->axis))
-		parse_error("Invalid cylinder axis", app);
+		parse_error("Invalid cylinder or cone axis", app);
 	skip_whitespace(line);
 	if (!parse_double(line, &s->diameter) || s->diameter <= 0)
-		parse_error("Invalid cylinder diameter", app);
+		parse_error("Invalid cylinder or cone diameter", app);
 	skip_whitespace(line);
 	if (!parse_double(line, &s->height) || s->height <= 0)
-		parse_error("Invalid cylinder height", app);
+		parse_error("Invalid cylinder or cone height", app);
 	skip_whitespace(line);
 	if (!parse_color(line, &s->color))
-		parse_error("Invalid cylinder color", app);
+		parse_error("Invalid cylinder or cone color", app);
 }
 
 bool	is_valid_axis(t_vector v)
 {
 	return (v.x >= -1.0 && v.x <= 1.0 && v.y >= -1.0 && v.y <= 1.0
-		&& v.z >= -1.0 && v.z <= 1.0);
+		&& v.z >= -1.0 && v.z <= 1.0
+		&& !(doubles_are_equal(v.x, 0)
+		&& doubles_are_equal(v.y, 0)
+		&& doubles_are_equal(v.z, 0)));
 }
 
 void	validate_normal_vector(t_vector normal, t_app *app)
@@ -43,6 +46,10 @@ void	validate_normal_vector(t_vector normal, t_app *app)
 		|| normal.y < -1.0 || normal.y > 1.0
 		|| normal.z < -1.0 || normal.z > 1.0)
 		parse_error("Plane normal must be normalized [-1,1]", app);
+	if ((doubles_are_equal(normal.x, 0)
+		&& doubles_are_equal(normal.y, 0)
+		&& doubles_are_equal(normal.z, 0)))
+		parse_error("Invalid plane normal", app);
 }
 
 void	validate_orientation_vector(t_vector ornt, t_app *app)
